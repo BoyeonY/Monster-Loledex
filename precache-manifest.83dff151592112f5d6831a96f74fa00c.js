eb4ed7562e1bc89449172767b67609d2c49b7712
Boyeon Yu <boyeon.yu@ibm.com>
2020-08-24 22:33:18 -0300

    Updates

---
+++ precache-manifest.83dff151592112f5d6831a96f74fa00c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc7539f0fb9c6399d0aeea66018060f",
+    "url": "/Monster-Loledex/index.html"
+  },
+  {
+    "revision": "28822ba284e5035f9fdd",
+    "url": "/Monster-Loledex/static/css/main.3611c2ba.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/Monster-Loledex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Loledex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28822ba284e5035f9fdd",
+    "url": "/Monster-Loledex/static/js/main.190ff02e.chunk.js"
+  },
+  {
+    "revision": "189e24377f0300199b2b",
+    "url": "/Monster-Loledex/static/js/runtime-main.6ad18cb2.js"
+  }
+]);
